707a3cc7b6178158b75e0476158425af0801765c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to customize mini map behavior and appearance
+
+
+<p> This example demonstrates how to customize mini map behavior and appearance.</p>
+<p> </p>
+<p>The Mini map supports two behavior types.<br />- <a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfMapFixedMiniMapBehaviortopic">Fixed</a>. The mini map's zoom level and a center point are fixed.<br />- <a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfMapDynamicMiniMapBehaviortopic">Dynamic</a>. The mini map's zoom level and a center point are based on the map's zoom level and center point.<br />To specify the behavior, assign the required behavior class to the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMiniMap_Behaviortopic">MiniMap.Behavior</a> property.<br /><br />In addition to basic WPF Control properties, the MiniMap class contains the following properties, which allow customizing its appearance: <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMiniMap_Alignmenttopic">Alignment</a>, <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMiniMap_ViewportTemplatetopic">ViewportTemplate</a>.</p>
+
+<br/>
+
+
---
+++ VB/MiniMapProperties/MainWindow.xaml.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Globalization
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace MiniMapProperties
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub ValidationError(ByVal sender As Object, ByVal e As ValidationErrorEventArgs)
+            If e.Action = ValidationErrorEventAction.Added Then
+                MessageBox.Show(e.Error.ErrorContent.ToString())
+            End If
+        End Sub
+    End Class
+
+    Public Class PositiveDoubleValidationRule
+        Inherits ValidationRule
+
+        Public Overrides Function Validate(ByVal value As Object, ByVal cultureInfo As CultureInfo) As ValidationResult
+            Dim val As Double = Nothing
+            If Double.TryParse(TryCast(value, String), NumberStyles.Number, cultureInfo, val) Then
+                If val <= 0 Then
+                    Return New ValidationResult(False, "Input value should be larger than 0.")
+                End If
+                Return New ValidationResult(True, Nothing)
+            End If
+            Return New ValidationResult(False, "Input value should be floating point number.")
+        End Function
+    End Class
+End Namespace
