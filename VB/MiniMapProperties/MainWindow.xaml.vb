21bb7a1627f167bcce05b48ea3d97149b6a75ee1
DevExpressExampleBot <fakeEmail>
2022-12-18 17:46:12 +0400

    Source auto update [skip ci]

--- VB/MiniMapProperties/MainWindow.xaml.vb
+++ VB/MiniMapProperties/MainWindow.xaml.vb
@@ -1,20 +1,18 @@
-﻿Imports System
 Imports System.Globalization
 Imports System.Windows
 Imports System.Windows.Controls
 
 Namespace MiniMapProperties
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub ValidationError(ByVal sender As Object, ByVal e As ValidationErrorEventArgs)
-            If e.Action = ValidationErrorEventAction.Added Then
-                MessageBox.Show(e.Error.ErrorContent.ToString())
-            End If
+            If e.Action = ValidationErrorEventAction.Added Then Call MessageBox.Show(e.Error.ErrorContent.ToString())
         End Sub
     End Class
 
@@ -22,13 +20,12 @@ Namespace MiniMapProperties
         Inherits ValidationRule
 
         Public Overrides Function Validate(ByVal value As Object, ByVal cultureInfo As CultureInfo) As ValidationResult
-            Dim val As Double = Nothing
+            Dim val As Double
             If Double.TryParse(TryCast(value, String), NumberStyles.Number, cultureInfo, val) Then
-                If val <= 0 Then
-                    Return New ValidationResult(False, "Input value should be larger than 0.")
-                End If
+                If val <= 0 Then Return New ValidationResult(False, "Input value should be larger than 0.")
                 Return New ValidationResult(True, Nothing)
             End If
+
             Return New ValidationResult(False, "Input value should be floating point number.")
         End Function
     End Class
